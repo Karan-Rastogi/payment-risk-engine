92b0ed3a844fc526497b44e6cc6727ae014808ee
Karan-Rastogi <krnrastogi18@gmail.com>
2026-09-21 15:08:10 +0530

    refactor: rename package to follow Java naming conventions

--- src/main/java/com/karan/risk/paymentriskengine/RiskEngineApplication.java
+++ src/main/java/com/karan/risk/paymentriskengine/RiskEngineApplication.java
@@ -1,4 +1,4 @@
-package com.karan.risk.payment_risk_engine;
+package com.karan.risk.paymentriskengine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
